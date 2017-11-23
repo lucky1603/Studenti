174f861188070a88ea16b15f1547a7b0430671f2
System Administrator <root@as-MacBook-Pro.local>
2017-11-23 11:03:13 +0100

    Baza update-ovana

--- database/studenti.sql
+++ database/studenti.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 10, 2017 at 11:20 AM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Host: localhost
+-- Generation Time: Nov 23, 2017 at 11:01 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `studenti`
 --
-CREATE DATABASE IF NOT EXISTS `studenti` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `studenti`;
 
 -- --------------------------------------------------------
 
@@ -28,7 +28,6 @@ USE `studenti`;
 -- Table structure for table `kursevi`
 --
 
-DROP TABLE IF EXISTS `kursevi`;
 CREATE TABLE `kursevi` (
   `id` int(11) NOT NULL,
   `student_id` int(11) NOT NULL COMMENT 'Referenca na tablu studenata',
@@ -50,15 +49,22 @@ CREATE TABLE `kursevi` (
   `napomene` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `kursevi`
+--
+
+INSERT INTO `kursevi` (`id`, `student_id`, `predmet_id`, `prisustvo`, `broj_casova`, `samostalni_zadaci`, `kolokvijum_1_poeni`, `kolokvijum_2_poeni`, `kolokvijum_1_datum`, `kolokvijum_2_datum`, `pismeni_datum`, `pismeni_poeni`, `usmeni_datum`, `usmeni_poeni`, `poeni_ukupno_do_usmenog`, `poeni_zbir`, `ocena`, `napomene`) VALUES
+(1, 8, 1, 0, 0, 0, 0, 0, '0000-00-00', '0000-00-00', '0000-00-00', 0, '0000-00-00', 0, 0, 0, 0, 'nisa specijalno');
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `predmeti`
 --
 
-DROP TABLE IF EXISTS `predmeti`;
 CREATE TABLE `predmeti` (
   `id` int(11) NOT NULL,
+  `sifra` varchar(10) NOT NULL,
   `ime` varchar(50) NOT NULL,
   `opis` text NOT NULL,
   `fond_casova` int(11) NOT NULL DEFAULT '0',
@@ -69,9 +75,9 @@ CREATE TABLE `predmeti` (
 -- Dumping data for table `predmeti`
 --
 
-INSERT INTO `predmeti` (`id`, `ime`, `opis`, `fond_casova`, `profesor`) VALUES
-(1, 'Osnovi taktike i gasenja pozara.', 'Kratak opis', 123, 'Barbara Vidakovic'),
-(2, 'Fizika', 'Osnove fizike', 150, 'Barbara Vidakovic');
+INSERT INTO `predmeti` (`id`, `sifra`, `ime`, `opis`, `fond_casova`, `profesor`) VALUES
+(1, '0302.11', 'Osnovi taktike i gasenja pozara.', 'Kratak opis', 123, 'Barbara Vidakovic'),
+(2, '0303.11', 'Fizika', 'Osnove fizike', 150, 'Barbara Vidakovic');
 
 -- --------------------------------------------------------
 
@@ -79,49 +85,27 @@ INSERT INTO `predmeti` (`id`, `ime`, `opis`, `fond_casova`, `profesor`) VALUES
 -- Table structure for table `studenti`
 --
 
-DROP TABLE IF EXISTS `studenti`;
 CREATE TABLE `studenti` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `ime` varchar(50) NOT NULL,
   `prezime` varchar(50) NOT NULL,
   `broj_indeksa` varchar(10) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `image` varchar(100) NOT NULL
+  `image` varchar(100) NOT NULL,
+  `firma` varchar(100) NOT NULL,
+  `telefon` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `studenti`
 --
 
-INSERT INTO `studenti` (`id`, `ime`, `prezime`, `broj_indeksa`, `email`, `image`) VALUES
-(8, 'Sinisa', 'Ristic', '34234234', 'sinisa.ristic@gmail.com', '/img/studenti/mala jaza.jpg'),
-(9, 'Barbara', 'Vidakovic', '3412341234', 'barbaravid@yahoo.co.uk', '/img/studenti/barbara.png'),
-(13, 'Jovan', 'Jovanovic', '2342341234', 'jovanjov@gmail.com', '/img/studenti/Patrick McCutcheon.jpg'),
-(14, 'Mira', 'Markovic', '2342141234', 'mmark@gmail.com', '/img/studenti/Nadja Spitzer.jpg'),
-(16, 'sfasdf', 'asdfasdfa', '5234523452', 'mma@gmail.com', '/img/studenti/Viola Gauci.jpg');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `studenti1`
---
-
-DROP TABLE IF EXISTS `studenti1`;
-CREATE TABLE `studenti1` (
-  `id` int(11) NOT NULL,
-  `ime` varchar(50) NOT NULL,
-  `prezime` varchar(50) NOT NULL,
-  `broj_indeksa` varchar(10) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `image` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `studenti1`
---
-
-INSERT INTO `studenti1` (`id`, `ime`, `prezime`, `broj_indeksa`, `email`, `image`) VALUES
-(0, 'Sinisa', 'Ristic', '242344', 'sinisa.ristic@gmail.com', 'mala jaza.jpg');
+INSERT INTO `studenti` (`id`, `ime`, `prezime`, `broj_indeksa`, `email`, `image`, `firma`, `telefon`) VALUES
+(8, 'Sinisa', 'Ristic', '34234234', 'sinisa.ristic@gmail.com', '/img/studenti/mala jaza.jpg', '', ''),
+(9, 'Barbara', 'Vidakovic', '3412341234', 'barbaravid@yahoo.co.uk', '/img/studenti/barbara.png', '', ''),
+(13, 'Jovan', 'Jovanovic', '2342341234', 'jovanjov@gmail.com', '/img/studenti/Patrick McCutcheon.jpg', '', ''),
+(14, 'Mira', 'Markovic', '2342141234', 'mmark@gmail.com', '/img/studenti/Nadja Spitzer.jpg', '', ''),
+(16, 'sfasdf', 'asdfasdfa', '5234523452', 'mma@gmail.com', '/img/studenti/Viola Gauci.jpg', '', '');
 
 --
 -- Indexes for dumped tables
@@ -132,14 +116,16 @@ INSERT INTO `studenti1` (`id`, `ime`, `prezime`, `broj_indeksa`, `email`, `image
 --
 ALTER TABLE `kursevi`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `student_id` (`student_id`),
-  ADD KEY `predmet_id` (`predmet_id`);
+  ADD UNIQUE KEY `student_predmet_uk` (`student_id`,`predmet_id`),
+  ADD KEY `predmet_id` (`predmet_id`),
+  ADD KEY `student_id` (`student_id`);
 
 --
 -- Indexes for table `predmeti`
 --
 ALTER TABLE `predmeti`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `sifra_uk` (`sifra`);
 
 --
 -- Indexes for table `studenti`
@@ -150,22 +136,24 @@ ALTER TABLE `studenti`
   ADD UNIQUE KEY `br_indeksa_email_uk` (`broj_indeksa`,`email`);
 
 --
--- Indexes for table `studenti1`
---
-ALTER TABLE `studenti1`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `br_indeksa_uk` (`broj_indeksa`),
-  ADD UNIQUE KEY `email_uk` (`email`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `kursevi`
+--
+ALTER TABLE `kursevi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `predmeti`
+--
+ALTER TABLE `predmeti`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `studenti`
 --
 ALTER TABLE `studenti`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- Constraints for dumped tables
 --
@@ -174,7 +162,9 @@ ALTER TABLE `studenti`
 -- Constraints for table `kursevi`
 --
 ALTER TABLE `kursevi`
-  ADD CONSTRAINT `kursevi_predmeti_fk` FOREIGN KEY (`predmet_id`) REFERENCES `predmeti` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `kursevi_fk_predmet` FOREIGN KEY (`predmet_id`) REFERENCES `predmeti` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `kursevi_fk_student` FOREIGN KEY (`student_id`) REFERENCES `studenti` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
